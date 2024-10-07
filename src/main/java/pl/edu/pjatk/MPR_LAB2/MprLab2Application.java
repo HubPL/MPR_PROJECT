2e98abc33f19678e064ef441513849d1d8ee42d3
Hubert Brzozowski <bobert2004.poczta@gmail.com>
2024-10-07 11:11:39 +0200

    Initial commit

---
+++ src/main/java/pl/edu/pjatk/MPR_LAB2/MprLab2Application.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.MPR_LAB2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MprLab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MprLab2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/pjatk/MPR_LAB2/MprLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.MPR_LAB2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MprLab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
